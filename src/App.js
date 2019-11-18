e374905c067127f813d4a68367eede5de1ca98b4
Aphelion <Aphelion01237@gmail.com>
2019-11-17 21:41:34 -0800

    working on frontpage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 
 import React from "react";
 import NavBar from "./components/NavBar";
+import IntroJumbo from './components/IntroLoggedOut'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
+    <body>
     <div className="App">
       {/* New - use BrowserRouter to provide access to /profile */}
       <BrowserRouter>
@@ -21,6 +24,8 @@ function App() {
         </Switch>
       </BrowserRouter>
     </div>
+    <IntroJumbo />
+    </body>
   );
 }
 
